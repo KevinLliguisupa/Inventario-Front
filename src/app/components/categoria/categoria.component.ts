bcb8bcc76e5e4c1ab44fda0d418ceae0124645ae
Alexis2701 <alexis.solano111@gmail.com>
2022-07-02 21:47:44 -0500

    `Crud Funcional Categoria

--- src/app/components/categoria/categoria.component.ts
+++ src/app/components/categoria/categoria.component.ts
@@ -22,7 +22,12 @@ export class CategoriaComponent implements OnInit {
   constructor(private categoriaService: CategoriaService, private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
-    this.cargarCategorias();
+    this.cargarCategorias()
+    this.form=this.formBuilder.group({
+      cat_id:[''],
+      cat_nombre:['']
+    })
+    
   }
 
   public cargarCategorias(){
@@ -33,6 +38,54 @@ export class CategoriaComponent implements OnInit {
       },
       (error)=>console.log(error)
     )
+  }
+
+  public crearCategoria() {
+    this.categoriaService.postCreateCat({
+      cat_nombre: this.form.value.cat_nombre,
+      cat_estado: this.form.value.cat_estado
+    }
+    ).subscribe(respuesta => {
+      console.log('Categoria enviada');
+      //Formulario reseteado
+      this.form.reset();
+      //Se cargue los datos despues de enviar
+      this.cargarCategorias();
+    }
+    );
+  }
+
+
+  public actualizarCategoria(cat_id: any) {
+    this.categoriaService.putUpdateCat({
+      cat_id: cat_id,
+      cat_nombre: this.form.value.cat_nombre,
+      cat_estado: this.form.value.cat_estado
+    }
+    ).subscribe(respuesta => {
+      //Se cargue los datos despues de enviar
+      console.log('Categoria editada correctamente.')
+      this.form.reset();
+      this.cargarCategorias()
+    }
+    );
+  }
+
+  public borrarCategoria(cat_id: any) {
+    this.categoriaService.deleteCat({
+      cat_id: cat_id
+    }).subscribe(res => {
+      console.log('Categoria eliminada correctamente.')
+      this.form.reset();
+      this.cargarCategorias()
+    })
+  }
+
+
+
+  public infoUpdateCategoria(cat_id: any, cat_nombre: any) {
+    this.informacionCategoria.cat_id = cat_id;
+    this.informacionCategoria.cat_nombre = cat_nombre;
   }
 
 }
--- src/app/service/categoria.service.ts
+++ src/app/service/categoria.service.ts
@@ -6,12 +6,38 @@ import {HttpClient, HttpHeaders} from '@angular/common/http'
 })
 export class CategoriaService {
 
-  apiUrl:string="https://api-modulo-inventario.herokuapp.com"
-
+  // apiUrl:string="https://api-modulo-inventario.herokuapp.com"
+  apiUrl:string="http://localhost:4000"
   constructor(private http:HttpClient) { }
   
   public getCategorias(){
     const url=this.apiUrl+`/categorias`
     return this.http.get(url)
+  }
+
+  // public getCategoriaById(pro_id:any){
+  //   const url = '/categorias/id/'+cat_id;
+  //   return this.http.get(url)
+  // }
+
+  // public getCategoriaByName(pro_nombre:any){
+  //   const url=this.apiUrl+`/categorias/nombre/`+cat_nombre;
+  //   return this.http.get(url)
+  // }
+
+  public postCreateCat(body:any){
+    const url=this.apiUrl+`/categorias/`
+    return this.http.post(url,body)
+  }
+
+  public putUpdateCat(body:any){
+    const url=this.apiUrl+`/categorias/`
+    return this.http.put(url,body)
+
+  }
+  public deleteCat(body:any){
+    const url=this.apiUrl+`/categorias/delete/`
+    return this.http.put(url,body)
+
   }
 }
