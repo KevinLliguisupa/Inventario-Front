fb4416fbcbfdf54eddd3118675c97b09ee8cb3b1
KevinLliguisupa <kevinlliguisupa6@gmail.com>
2022-07-22 02:03:25 -0500

    despliegue

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/Inventario-Front'));
+app.use(express.static('./dist/inventario-front'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/Inventario-Front/'}),
+    res.sendFile('index.html', {root: 'dist/inventario-front/'}),
 );
 
 // Start the app by listening on the default Heroku port
