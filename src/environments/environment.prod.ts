7c3d58dd3f81f8588e31d94c83142862ad118cd0
KevinLliguisupa <kevinlliguisupa6@gmail.com>
2022-07-22 03:42:02 -0500

    Version despliegue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
-  apiBaseURL: 'https://api-modulo-inventario.herokuapp.com'
+  firebaseConfig :{
+    apiKey: "AIzaSyDGK6KSISNEcGKN6xUUqTDZd-4MlO6SqSk",
+    authDomain: "app-productos-image.firebaseapp.com",
+    projectId: "app-productos-image",
+    storageBucket: "app-productos-image.appspot.com",
+    messagingSenderId: "523520135609",
+    appId: "1:523520135609:web:b997e9474af2361bb0a668",
+    measurementId: "G-28Y3PCHRM4"
+  }
 };
